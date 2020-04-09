0dc519d3cf9ee454969f330a23c78e7ab7b5824e
Luis Lemos <49795032+OmegaSmash@users.noreply.github.com>
2020-04-09 09:12:32 +0100

    Update controller.c

--- Controller/controller.c
+++ Controller/controller.c
@@ -2,10 +2,12 @@
 #include "..\models\paises.h"
 #include "..\models\cursos.h"
 #include "..\models\unidadesCurriculares.h"
+#include "..\models\alunos.h"
 
 #include "..\views\vwPaises.h"
 #include "..\views\vwCursos.h"
 #include "..\views\vwUnidadesCurriculares.h"
+#include "..\views\vwAlunos.h"
 
 Listagens CarregaListagens() {
 	Listagens listagens;
@@ -13,7 +15,8 @@ Listagens CarregaListagens() {
 	listagens.paises = PaisCarregaTodos();
 	listagens.cursos = CursoCarregaTodos();
 	listagens.unidadesCurriculares = UnidadeCurricularCarregaTodos();
-	
+	listagens.alunos = AlunoCarregaTodos();
+
 	return listagens;
 }
 
@@ -21,6 +24,7 @@ void MostraListagens(Listagens listagens) {
 	PaisMostraLista(listagens.paises);
 	CursoMostraLista(listagens.cursos);
 	UnidadesCurricularesMostraLista(listagens.unidadesCurriculares, listagens);
+	AlunoMostraLista(listagens.alunos);
 }
 
 void MostraUnidadesCurricularesCurso(Listagens listagens, int curso) {
